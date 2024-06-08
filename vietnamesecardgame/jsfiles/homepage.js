73719a299ecc8c17f497adf721c990a126423277
phuongwj <phamjulia9@gmail.com>
2024-06-07 22:38:05 -0300

    first commit

---
+++ vietnamesecardgame/jsfiles/homepage.js
@@ -0,0 +1,10 @@
+const play = document.getElementById("play");
+const how = document.getElementById("how to play");
+
+play.addEventListener("click", () => {
+    window.location.href = "main.html";
+});
+
+how.addEventListener("click", () => {
+    window.location.href = "how.html";
+})
